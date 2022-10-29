2f903ade64fecdf6809ca9d79ca212572768880d
wangweikai <513547585@qq.com>
2022-10-30 00:34:50 +0800

    启动
    Signed-off-by: wangweikai <513547585@qq.com>

--- src/main/java/com/itheima/MySpringBootApplication.java
+++ src/main/java/com/itheima/MySpringBootApplication.java
@@ -3,7 +3,7 @@ package com.itheima;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//启动20221029--wwk00000
+//启动20221029--wwk123456978
 @SpringBootApplication
 public class MySpringBootApplication {
     public static void main(String[] args) {
